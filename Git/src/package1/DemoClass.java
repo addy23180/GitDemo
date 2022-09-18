8efe4262e407a29a1ff2dc12b1ec1f871350c963
shete <advitashete1726@gmail.com>
2022-09-18 19:59:11 +0530

    Second comment

--- Git/src/package1/DemoClass.java
+++ Git/src/package1/DemoClass.java
@@ -1,8 +1,15 @@
 package package1;
 
 public class DemoClass {
+	public void emma() {
+		System.out.println("Hello, this is Emma");
+	}
 	public static void main(String [] args) {
-		System.out.println("Hello Git");
+		
+		System.out.println("Hello Git, this is Advita");
+		DemoClass t = new DemoClass();
+		t.emma();
+		
 	}
 
 }
